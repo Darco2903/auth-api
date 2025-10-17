60f071dddc56cf9e990434fe58ea617d4768a19b
Darco2903 <william.valenduc@orange.fr>
2025-10-17 17:03:53 +0200

    getRoleName getRoleLevel
    package update

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,19 @@
 export * from "./common.js";
 import { initClient } from "@ts-rest/core";
 import contract from "./contract/index.js";
+import { UserRole } from "./roles.js";
 
 export function createClient(origin: string) {
     return initClient(contract, {
         baseUrl: origin,
         credentials: "include",
     });
+}
+
+export function getRoleName(level: number): string {
+    return UserRole[level] || "Unknown";
+}
+
+export function getRoleLevel(name: string): number {
+    return UserRole[name as keyof typeof UserRole] || -1;
 }
