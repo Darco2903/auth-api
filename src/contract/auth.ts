7cacd25abf125acc1ca7228f11a7f87f657311b0
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2026-01-08 02:25:13 +0100

    totpDisable + totpSetup 400 TOTP_ALREADY_SETUP

--- src/contract/auth.ts
+++ src/contract/auth.ts
@@ -97,7 +97,10 @@ export default c.router({
                     otpauthUrl: z.string(),
                 })
             ),
-            400: ZodErrorSchema,
+            400: z.union([
+                ZodErrorSchema,
+                apiError(z.literal("TOTP_ALREADY_SETUP"), z.string()),
+            ]),
             401: apiError(z.literal("UNAUTHORIZED"), z.literal("Unauthorized")),
             500: apiError(z.literal("INTERNAL_SERVER_ERROR"), z.string()),
         },
@@ -135,6 +138,25 @@ export default c.router({
                 ZodErrorSchema,
                 apiError(z.literal("TOTP_NOT_SETUP"), z.string()),
                 apiError(z.literal("TOTP_NOT_REQUIRED"), z.string()),
+                apiError(z.literal("TOTP_INVALID"), z.string()),
+            ]),
+            401: apiError(z.literal("UNAUTHORIZED"), z.literal("Unauthorized")),
+            500: apiError(z.literal("INTERNAL_SERVER_ERROR"), z.string()),
+        },
+    },
+
+    totpDisable: {
+        method: "POST",
+        path: "/totp/disable",
+        headers: authHeaderSchema,
+        body: z.object({
+            totpCode: totpCodeSchema,
+        }),
+        responses: {
+            200: apiSuccess(c.noBody()),
+            400: z.union([
+                ZodErrorSchema,
+                apiError(z.literal("TOTP_NOT_SETUP"), z.string()),
                 apiError(z.literal("TOTP_INVALID"), z.string()),
             ]),
             401: apiError(z.literal("UNAUTHORIZED"), z.literal("Unauthorized")),
