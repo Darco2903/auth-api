f4618e40c4a543c3f4ac2362997f543cc5c0263e
Darco2903 <114527594+Darco2903@users.noreply.github.com>
2026-01-17 19:51:12 +0100

    password requirements

--- src/types/creds.ts
+++ src/types/creds.ts
@@ -16,32 +16,33 @@ export const emailCredentialSchema = emailSchemaBase.email({
     message: "Invalid Credentials",
 });
 
-const REGEX_LOWER = /[a-z]/; // contains at least one lowercase letter
-const REGEX_UPPER = /[A-Z]/; // contains at least one uppercase letter
-const REGEX_NUMBER = /[0-9]/; // contains at least one number
-const REGEX_SPECIAL = /[" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]/; // contains at least one special character
+// const REGEX_LOWER = /[a-z]/; // contains at least one lowercase letter
+// const REGEX_UPPER = /[A-Z]/; // contains at least one uppercase letter
+// const REGEX_NUMBER = /[0-9]/; // contains at least one number
+// const REGEX_SPECIAL = /[" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]/; // contains at least one special character
 
 export const passwordSchema = z
     .string()
-    .regex(REGEX_LOWER, "Password must contain at least one lowercase letter")
-    .regex(REGEX_UPPER, "Password must contain at least one uppercase letter")
-    .regex(REGEX_NUMBER, "Password must contain at least one number")
-    .regex(
-        REGEX_SPECIAL,
-        "Password must contain at least one special character"
-    )
+    // .regex(REGEX_LOWER, "Password must contain at least one lowercase letter")
+    // .regex(REGEX_UPPER, "Password must contain at least one uppercase letter")
+    // .regex(REGEX_NUMBER, "Password must contain at least one number")
+    // .regex(
+    //     REGEX_SPECIAL,
+    //     "Password must contain at least one special character"
+    // )
     .min(PASSWORD_MIN_LENGTH, "Password is too short")
     .max(PASSWORD_MAX_LENGTH, "Password is too long");
 
 export const passwordCredentialSchema = z.string().superRefine((val, ctx) => {
-    const hasLower = REGEX_LOWER.test(val);
-    const hasUpper = REGEX_UPPER.test(val);
-    const hasNumber = REGEX_NUMBER.test(val);
-    const hasSpecial = REGEX_SPECIAL.test(val);
+    // const hasLower = REGEX_LOWER.test(val);
+    // const hasUpper = REGEX_UPPER.test(val);
+    // const hasNumber = REGEX_NUMBER.test(val);
+    // const hasSpecial = REGEX_SPECIAL.test(val);
     const validLength =
         val.length >= PASSWORD_MIN_LENGTH && val.length <= PASSWORD_MAX_LENGTH;
 
-    if (!(hasLower && hasUpper && hasNumber && hasSpecial && validLength)) {
+    // if (!(hasLower && hasUpper && hasNumber && hasSpecial && validLength)) {
+    if (!validLength) {
         ctx.addIssue({
             code: z.ZodIssueCode.custom,
             message: "Invalid Credentials",
